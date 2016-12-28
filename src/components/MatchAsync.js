30943af71c3b8eaf97a0dfde7552b86ba0d3ae32
maxiaochuan <mxcins@gmail.com>
2016-12-28 23:23:11 +0800

    how async

---
+++ src/components/MatchAsync.js
@@ -0,0 +1,43 @@
+'use strict'
+import React, { Component, PropTypes } from 'react'
+import { Match } from 'react-router'
+
+class MatchAsync extends Component {
+  static propTypes = {
+    getComponent: PropTypes.func.isRequired,
+  }
+
+  constructor (...args) {
+    super(...args)
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  getComponent = async () => {
+    const { getComponent } = this.props
+    try {
+      this.component = await getComponent()
+      this.setState({
+        loaded: true,
+      })
+    } catch (error) {
+      throw error
+    }
+  }
+
+  render () {
+    return (
+      <Match render={matchProps => {
+          const { component, getComponent } = this.props
+          if(component === undefined) {
+            getComponent()
+          }
+          return component !== undefined ? React.createEelment(component, matchProps) :
+            null
+        }}/>
+    )
+  }
+}
+
+export default MatchAsync
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,37 @@ import NumberContainer from './Number'
 import WelcomeContainer from './Welcome'
 import App from '../components/App'
 import Match from 'react-router/Match'
+import MatchAsync from '../components/MatchAsync'
+
+const routeMap = [
+  {
+    pattern: '/',
+    exactly: true,
+    component: HomeContainer,
+  },
+]
+
+const asyncRouteMap = [
+  {
+    pattern: 'number',
+    getComponent () {
+      System.import('./Number')
+    },
+  },
+]
 
 export default props => (
   <App {...props}>
-    <Match exactly pattern="/" component={HomeContainer} />
-    <Match pattern="/number" component={NumberContainer} />
-    <Match pattern="/welcome" component={WelcomeContainer} />
+    {routeMap.map((route, i) => (<Match key={i} {...route}/>))}
+    {!process.env.BROWSER ? <span/> : asyncRouteMap.map((route, i) =>
+      (<MatchAsync key={i} {...route}/>))}
   </App>
 )
+
+// export default props => (
+//   <App {...props}>
+//     <Match exactly pattern="/" component={HomeContainer} />
+//     <Match pattern="/number" component={NumberContainer} />
+//     <Match pattern="/welcome" component={WelcomeContainer} />
+//   </App>
+// )
